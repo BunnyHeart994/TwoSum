ff489e0573ffd11315b69686e724e8a3f0d52600
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2025-01-07 16:20:25 -0300

    Work in progress

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] nums = new int[] {3, 4, 5, 6};
+        System.out.println(Solution.twoSum(nums, 10));
     }
 }
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[nums.length];
+        for (short i = 0; i < nums.length; i++) {
 
+        }
     }
 }
