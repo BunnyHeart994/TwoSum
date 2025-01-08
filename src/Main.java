99582a2fc9523aa56548f207fee7dc84ee92b58b
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2025-01-08 15:10:09 -0300

    v1.0 Done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.Arrays;
 
+public class Main {
+    protected static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Henrique's TwoSum v1.0\nWelcome. This program finds the index of 2 numbers " +
+                "in a sequence,\nthat if added up together, result in the target number.\nThe sequence " +
+                "and the target are user defined.\n");
+        System.out.print("How many numbers do you want?\n--> ");
+        short arrLength = scan.nextShort();
+        int[] arr = new int[arrLength];
+        System.out.println("Insert the numbers:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print((i + 1) + " -> ");
+            arr[i] = scan.nextShort();
+        }
+        System.out.print("Target number: ");
+        short target = scan.nextShort();
+        System.out.println();
+
+        System.out.println("Sequence: " + Arrays.toString(arr) + "\nIndexes: " +
+                Arrays.toString(Solution.twoSum(arr, target)) + "\n");
+
+        System.out.println("Thank you for using. Reopen the program to do it again.");
     }
 }
 
@@ -11,7 +35,7 @@ class Solution {
             for (int j = 1; j < nums.length; j++) {
                 if (result [0] != 0)
                     break;
-                if (nums[i] + nums[j] == target) {
+                if (nums[i] + nums[j] == target && nums[i] != nums[j]) {
                     result[0] = i;
                     result[1] = j;
                 }
